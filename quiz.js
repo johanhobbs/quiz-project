af8aa6183c7583eb231d3b342c7ed0e551e51f83
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-03-13 12:24:28 +1300

    progressed this code more

--- quiz.js
+++ quiz.js
@@ -1,1 +1,28 @@
-// Install Node.js
+// NOTE - Installed Node.js
+
+// NOTE - Install Readline - later
+
+const questions = [
+  {
+    question: "What is 2 + 2?",
+    options: ["2", "3", "4", "5"],
+    answer: "4",
+  },
+  {
+    question: "What is the capital of New Zealand?",
+    options: ["Auckland", "Wellington", "Nelson", "Christchurch"],
+    answer: "Wellington",
+  },
+  // TODO - Add more questions here.
+];
+
+// Declare quiz function
+function quiz() {
+  let score = 0;
+
+  //Iterate through questions
+  questions.forEach((q, index) => {
+    console.log(`Question ${index + 1}: ${q.question}`);
+    // q in this case points to the currently active item in the "questions" array that is currently active
+  });
+}
